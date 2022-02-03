8de3626b47bd0ad359b42f6d2b8fc393bdf70037
KwakYV <kvakyur@gmail.com>
2022-02-03 21:29:00 +0300

    first commit

---
+++ src/main/java/ru/gb/gbfront/GbFrontApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.gbfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/gbfront/GbFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.gbfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
