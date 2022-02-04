01ecda9b48ec7684337caca15800de1aecb9aed4
KwakYV <kvakyur@gmail.com>
2022-02-04 21:43:33 +0300

    Homework5 - Подключение интерфейса для Product.

---
+++ src/main/java/ru/gb/gbfront/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package ru.gb.gbfront.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests((request)->{
+            request.antMatchers("/", "/product/all", "/auth").permitAll();
+            request.antMatchers(HttpMethod.POST, "/product").hasRole("ADMIN");
+        });
+
+        http.authorizeRequests((requests) -> {
+            ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+        });
+        http.formLogin();
+        http.httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
